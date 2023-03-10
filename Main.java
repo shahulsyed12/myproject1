d691ee5a587b3a2a15fdd252a3ccbc62e0ecc7b3
shahulsyed12 <127485485+shahulsyed12@users.noreply.github.com>
2023-03-10 16:33:38 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,19 +1,28 @@
 import java.util.Scanner;
-class Add
-{
-    public static void main(String[] arg)
-    {
-        int a,b,c;
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter first number");
-        a=sc.nextInt();
-        System.out.println("Enter second number");
-        b=sc.nextInt();
-        c=addition(a,b);
-        System.out.println(" Addition of two numbers is : "+c);
-    }
-    static int addition(int x,int y)
-    {
-        return x+y;
+
+public class Main{
+
+    public static void main(String[] args) {
+
+        /* This reads the input provided by user
+         * using keyboard
+         */
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+
+        // This method reads the number provided using keyboard
+        int num1 = scan.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = scan.nextInt();
+
+        // Closing Scanner after the use
+        scan.close();
+
+        // Calculating product of two numbers
+        int product = num1*num2;
+
+        // Displaying the multiplication result
+        System.out.println("Output: "+product);
     }
 }
